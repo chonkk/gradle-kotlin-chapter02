a0cf50724530ded5df5924f263cea8145d8c8d4f
천광규 <chonkk@castis.com>
2021-06-29 19:52:34 +0900

    Update chapter2

--- src/main/kotlin/com/chonkk/al/Application.kt
+++ src/main/kotlin/com/chonkk/al/Application.kt
@@ -1,10 +1,57 @@
 package com.chonkk.al
 
-
 fun main() {
-    sample3()
-    sample2()
-    sample1()
+    println(kotlinTypeException(true))
+    println(kotlinTypeException(false))
+//    println(kotlinTye("또 날세", 21))
+//    println(javaType("날세", 20))
+//    println(createMemoFor2("eee"))
+//    println(createMemoFor("eee"))
+    //sample3()
+    //sample2()
+//    sample1()
+}
+fun kotlinTypeException(status: Boolean): Int {
+    return try {
+        if (status) {
+            throw RuntimeException()
+        }
+        1
+    } catch (e: Exception) {
+        2
+    }
+}
+fun kotlinTye(name: String, age: Int): String {
+    val status = if (age < 17) "어려" else "많아"
+    return "$name, $status"
+}
+fun javaType(name: String, age: Int): String {
+    var status: String
+    if (age < 17) {
+        status = "어려"
+    } else {
+        status = "많아"
+    }
+
+    return "$name, $status"
+}
+fun createMemoFor2(name: String): String {
+    if (name == "eee") {
+        val memo = """asdfasfasdfasdfasdf
+        | asdfasdfasdf
+        | sadfasdfasdfasdfasdf"""
+        return memo.trimMargin()
+    }
+    return ""
+}
+fun createMemoFor(name: String): String {
+    if (name == "eee") {
+        val memo = """asdfasfasdfasdfasdf
+        asdfasdfasdf
+        sadfasdfasdfasdfasdf"""
+        return memo
+    }
+    return ""
 }
 
 fun sample3() {
